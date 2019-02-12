a0818c48d60499213fd577e6cab90b75d9681bcd
Janchorizo <id00681749@usal.es>
2019-02-12 18:57:35 +0100

    Added axis to ViolinPlot component

--- client/src/components/vis/ViolinPlot.js
+++ client/src/components/vis/ViolinPlot.js
@@ -152,13 +152,14 @@ class ViolinPlot extends React.Component{
             .domain([-maxnum,maxnum])
             .range([0,this.props.width-params.margin*2-params.padding*2]);
 
-        const yAxis = g => g
-            .attr("transform", `translate(${params.padding},0)`)
-            .call(d3.axisLeft(yScale))
-            .call(g => g.select(".domain").remove());
 
         d3.select(this.vis)
             .attr('transform', `translate(${params.padding + 35},0)`);
+        
+        const yAxis = g => g
+            .attr("transform", `translate(${params.padding},0)`)
+            .call(d3.axisRight(yScale).tickSize(width-params.padding))
+            .call(g => g.select(".domain").remove());
 
         d3.select(this.vis)
             .select('path')
@@ -173,7 +174,14 @@ class ViolinPlot extends React.Component{
             );
 
         d3.select(this.axis)
-              .call(yAxis);
+              .call(customYAxis);
+
+        function customYAxis(g) {
+          g.call(yAxis);
+          g.select(".domain").remove();
+          g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
+          g.selectAll(".tick text").attr("x", 4).attr("dy", -4);
+        }
     }
 
     selectAttribute(attr){
