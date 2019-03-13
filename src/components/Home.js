cd78b7ee98bc2e4669911a475543b60347e27a89
Janchorizo <jancho@usal.es>
2019-03-13 20:28:41 +0100

    Added description and logos to the home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,27 @@ import { BrowserRouter as Route, NavLink } from "react-router-dom";
  */
 const Home = (props)=>{
     return(<div id="home">
-        <h2>ExploreAT DH Dashboard</h2>
-
+        <h3>ExploreAT DH Dashboard</h3>
+        <h5>Visually exploring RDF data from the perspective of an ontology</h5>
+        <h5>
+            Query a SPARQL endpoint using an ontology schema as a reference<br/>
+            and build a visual dashboard to see the underlying information.<br/>
+        </h5>
         <div className="banner">
             <img src="/public/step3.png"/>
-            <img src="/public/step-9.png"/>
+            <img src="/public/step-8.png"/>
             <NavLink to="/entities"><h2>Start the app</h2></NavLink>
+        </div>
+        <h5>
+            This application is a result framed in the project ExploreAT!. A project<br/>
+            aimed at providing open science and citizen science techniques to<br/>
+            leverage knowledge and unique insights into the German Language.<br/>
+        </h5>
+        <div id="logo-container">
+            <img id="exploreat" src="/public/square_logo.png"/>
+            <img id="visusal" src="/public/visusal.png"/>
+            <img id="usal" src="/public/usal.jpg"/>
+            <img id="acdh" src="/public/acdh.png"/>
         </div>
       </div>);
 }
