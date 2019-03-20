fe4887249d5424cecdaf706752531dd7dd875129
Janchorizo <jancho@usal.es>
2019-03-20 10:32:33 +0100

    Added secure measure to aviod firing two node selection events upon double click whithout waiting the updates causing the crashes.

--- src/components/EntityForceLayout.js
+++ src/components/EntityForceLayout.js
@@ -27,6 +27,8 @@ class EntityForceLayout extends React.Component{
 			loaded: false, 
 		};
 
+        this.selectEntity = [this.props.selectEntity,];
+
 		this.sparqlQueries = new SparqlQueryBuilder();
 		this.wrapper = new UrlParamWrapper();
 		this.updateHighlights = this.updateHighlights.bind(this);
@@ -44,6 +46,8 @@ class EntityForceLayout extends React.Component{
 	componentDidUpdate(prevProps, prevState, snapshot){
 		if(this.state.loaded === true){
             this.updateHighlights(this.props);
+            if(this.props.test_nodes.length==0 && this.selectEntity.length==0)
+                this.selectEntity.push(this.props.selectEntity);
         }
 	}
 
@@ -157,7 +161,9 @@ class EntityForceLayout extends React.Component{
 			.attr('class', 'node')
 			.attr('id', d=>this.wrapper.nameOfEntity(d.entity))
 			.on("click",(d)=>{
-				this.props.selectEntity(d.entity)
+                if(this.props.test_nodes.length == 0 && this.selectEntity.length==1){
+                    this.selectEntity.pop()(d.entity);
+                }
 			})
 			.call(layout.drag);
 
