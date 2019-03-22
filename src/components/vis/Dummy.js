b2f0b853f096ecc8f50a671aaceb05abe2bef98a
Janchorizo <jancho@usal.es>
2019-03-22 10:19:22 +0100

    Redone.

--- src/components/vis/Dummy.js
+++ src/components/vis/Dummy.js
@@ -1,11 +1,12 @@
 import * as d3 from 'd3';
 import React from 'react';
 
-/* StreamGraph
- * StreamGraph visualization for representing the evolution or distribution 
- * of an aggregation over a variable
+/* Dummy
+ * Dummy component for scaffolding vis components
+ * Vis components are provided with width, height and data props
  * 
- * It must receive at least one aggregated attribute and another one not aggregated
+ * Data is provided as an array of objects, each of wich is an entry with 
+ * a key and value for each of the attributes. 
  *
  * The component updates each time the data is been filtered, or the size of the
  * container changes.
@@ -36,35 +37,30 @@ const params = {
     paddingBottom: 10,
  };
 
-class CirclePacking extends React.Component{
+class Dummy extends React.Component{
     constructor(props){
         super(props);
-        // Auxiliary functions to manage strings
-        this.stripUri = (value)=>String(value).includes('/')?value.split('/')[value.split('/').length-1]:value;
-        this.sanitizeClassName = (name)=>(name.replace(/"/g,'').replace(/\./g,'').replace(/ /g, ''));
-        this.updateData = this.updateData.bind(this);
+        this.updatedData = this.updatedData.bind(this);
 
-        const state = {
-            hierarchy: this.props.attributes,
-            data: this.updateData(props.data,
-                this.props.attributes),
-            hierarchyChanged: false
+        this.state = {
+            sector_dimension:"",
+            data: null,
+            total: 1,
+            sortBy: 'key',
+            keySortOrder:'up',
+            valueSortOrder:'up',
+            sortingFunction: this.sortingFunctions['key']['up'],
         };
 
-        this.state = state;
-
         this.node = d3.select(this.node);
-        this.moveAttribute = this.moveAttribute.bind(this);
+        this.selectAttribute = this.selectAttribute.bind(this);
         this.highlightEntities = this.highlightEntities.bind(this);
         this.unhighlightEntities = this.unhighlightEntities.bind(this);
         this.filterBySomeAttribute = this.filterBySomeAttribute.bind(this);
         this.setSortBy = this.setSortBy.bind(this);
-        this.renderCirclePacking = this.renderCirclePacking.bind(this);
-
     }
 
     componentDidMount(){
-        this.renderCirclePacking();
     }
 
     componentWillUnmount(){
@@ -79,109 +75,30 @@ class CirclePacking extends React.Component{
         shouldUpdate = shouldUpdate || (nextProps.height != this.props.height);
         shouldUpdate = shouldUpdate || (nextProps.data != this.props.data);
         shouldUpdate = shouldUpdate || (nextState.data != this.state.data);
-        shouldUpdate = shouldUpdate || (nextState.hierarchyChanged === true);
+        shouldUpdate = shouldUpdate || (nextState.selected_attribute != this.state.selected_attribute);
 
         return shouldUpdate;
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if(prevProps.data != this.props.data || this.state.hierarchyChanged===true){
-            const data = this.updateData(this.props.data,
-                this.state.hierarchy);
-            this.setState({data, hierarchyChanged:false}, this.renderCirclePacking);
+        if(prevProps.data != this.props.data){
+            const {data, total} = this.updatedData(this.state.selected_attribute);
+            this.setState({data,total});
         }
-        if(prevProps.width != this.props.width || prevProps.height != this.props.height){
-            this.renderCirclePacking()
-        }
+        if(prevProps.width != this.props.width){}
+        if(prevProps.height != this.props.height){}
     }
 
-    updateData(data, hierarchy){
-        const results = [];
-        
-        const parents = new Map();
-        parents.set('root', {id:'root',size:null});
+    selectAttribute(attribute){
+        const {data, total} = this.updatedData(attribute);
 
-        for(let d of data){
-            let values = {};
-            results.push(
-                {id: hierarchy
-                    .map(x=>d[x.attribute])
-                    .reduce((a,b,i)=>{
-                        const accumulated = a+(a==''?'':'.')+this.sanitizeClassName(this.stripUri(String(b)));
-                        if(i<hierarchy.length-1 && parents.get(accumulated) == undefined)
-                            parents.set(accumulated,{id:accumulated,size:null});
-                        return accumulated;
-                    },"root"),
-                size:1
-                }
-            );
-        }
-
-        return Array.prototype.concat(Array.from(parents.values()),results);
-    }
-
-    renderCirclePacking(){
-        const stripUri = this.stripUri,
-            sanitizeClassName = this.sanitizeClassName,
-            height = this.props.height-params.marginTop,
-            width = this.props.width-params.marginRight-params.legendWidth;
-
-        const stratify = d3.stratify()
-            .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
-
-        const pack = d3.pack()
-            .size([width - params.paddingRight - params.paddingLeft
-                , height - params.paddingBottom - params.paddingTop])
-            .padding(4);
-
-        const vData = stratify(this.state.data);
-
-        let vLayout = d3.pack().size([width, height]);
-
-        // Layout + Data
-        const vRoot = d3.hierarchy(vData).sum(function (d) { return d.data.size; });
-        const vNodes = vRoot.descendants();
-        vLayout(vRoot);
-        
-        let g = d3.select(this.svg).select('g#vis');
-        
-        let vSlices = g.selectAll('g').data(vNodes);
-        vSlices.exit().remove();
-        vSlices.enter().append('g').each(function(d){
-            const node = d3.select(this);
-            node.append('title');
-            node.append('circle');
-        });
-
-        vSlices = g.selectAll('g');
-        const componentRef = this;
-        // Draw on screen
-        vSlices.each(function(d){
-            const node = d3.select(this);
-            node.select('title')
-                .text(d=>d.data.id.split('.')[d.data.depth]);
-
-            node.select('circle')
-            .attr('class', d=>d.data.depth==0?'':`${componentRef.state.hierarchy[d.data.depth-1]['attribute']}-${d.data.id.split('.')[d.data.depth]}`)
-            .on("mouseover", componentRef.highlightEntities)
-            .on("mouseout", componentRef.unhighlightEntities)
-            .style('fill',d=>d.data.depth==0
-                ?'white'
-                :componentRef.props.colorScales[componentRef.state.hierarchy[d.data.depth-1]['attribute']](d.data.id.split('.')[d.data.depth]))
-            .transition()
-                .attr('cx', function (d) { return d.x; })
-                .attr('cy', function (d) { return d.y; })
-                .attr('r', function (d) { return d.r; });
-        });
-    }
-
-    moveAttribute(prevIndex,newIndex){
-        this.setState(prev=>{
-            const t = prev.hierarchy.splice(prevIndex,1)[0];
-            prev.hierarchy.splice(newIndex,0,t);
-            prev.hierarchyChanged=true;
-            return(prev);
-        });
+        this.setState({
+            data, 
+            total,
+            legend: attribute[attribute.aggregation_term!='none'?'aggregation_term':'name'],
+            sector_dimension:attribute.name, 
+            selected_attribute: attribute,
+        })
     }
 
     // Example of to use filtering
@@ -190,11 +107,8 @@ class CirclePacking extends React.Component{
         this.props.updateFilteredData()
     }
 
-    highlightEntities(d){
-        if(d.data.depth != 0){
-            const selector = `.${this.state.hierarchy[d.data.depth-1]['attribute']}-${d.data.id.split('.')[d.data.depth]}`;
-            d3.selectAll(selector).classed('hovered',true);
-        }
+    highlightEntities(selector){
+        d3.selectAll('.'+selector).classed('hovered',true);
     }
 
     unhighlightEntities(d){
@@ -223,32 +137,14 @@ class CirclePacking extends React.Component{
         }
         
         return(
-            <div id="CirclePacking" className="visualization" style={size} ref={node => this.domElement = node}>
-                <svg ref={node => this.svg = node} 
-                    width={this.props.width-params.marginRight-params.legendWidth} 
-                    height={this.props.height-params.marginTop}>
-                    <g id="vis" transform={`translate(${params.paddingLeft},${params.paddingTop})`}></g>
-                </svg>
-                <div className="menu">
-                {this.state.hierarchy.map((x,i)=>(
-                    <div key={x.attribute}>
-                        <span >
-                        {x.attribute} 
-                        {i>0
-                            ?<span className="button" onClick={()=>this.moveAttribute(i,i-1)}> up </span>
-                            :""}
-                        {i<this.state.hierarchy.length-1
-                            ?<span className="button" onClick={()=>this.moveAttribute(i,i+1)}> down </span>
-                            :""}
-                        </span>
-                        <br/>
-                    </div>
-                ))
-                }
-                </div>
+            <div id="Dummy" className="visualization" style={size} ref={node => this.domElement = node}>
+                <p style={{margin:0}}>Dummy component</p>
+                <p style={{margin:0}}>Select the attribute used for the sectors : {this.props.attributes.map(e=>(
+                    <span key={e.name} onClick={()=>this.selectAttribute(e)} className="option" style={style(e.name)}> {e.name} </span>
+                ))}</p>
             </div>
         );
     }
 }
 
-export default CirclePacking;
+export default Dummy;
